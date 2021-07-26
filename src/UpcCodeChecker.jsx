b684d6839877a94494a7654076736fdbf93aa833
ppsk2011 <ppsk2011@gmail.com>
2021-07-26 12:42:37 +0530

    checksum css

--- src/UpcCodeChecker.jsx
+++ src/UpcCodeChecker.jsx
@@ -27,7 +27,7 @@ const UpcCodeChecker = () => {
                 <div>Odd digits sum multiplied by 3 : {item.multipliedSum}</div>
                 <div>Sum of even digits : {item.evenSum}</div>
                 <div> Even digits + multiplied odd digits: {item.addedSum}</div>
-                <div> Calculated check Digit to compare with last number {item.checkDigit}</div>
+                <div className='validation'> Calculated checksum Digit : {item.checkDigit}</div>
                 </div>
 
             ))}
